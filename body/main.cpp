e5683a2b72a7440625dfaa41b769ea353e7fae5e
Pierre Kettmus <pkettmus@gmail.com>
2018-03-21 09:54:01 +0100

    Initial Commit

---
+++ body/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  body
+//
+//  Created by Pierre Kettmus on 21/03/2018.
+//  Copyright © 2018 Pierre Kettmus. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
